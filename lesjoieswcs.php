356e6e897c51183107e0c1fceccf8e34e7fa4d93
Alima Garcia Stéphane <36844486+garciastephane@users.noreply.github.com>
2018-03-14 13:22:32 +0100

    Add files via upload

---
+++ lesjoieswcs.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Lesjoiedelawcs</title>
+    <link href="style.css" rel="stylesheet" type="text/css"
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styleprojet.css" />
+  </head>
+
+<body>
+
+
+<div class="container-fluid">
+
+  <!-- add comments -->
+  <!-- 1ere ligne-->
+
+  <div class="row">
+    <div class="col-xs-12 col-md-6 texte1">
+      <p>Si dans cinq mois tu ne sais toujours pas coder t'auras au moins appris à
+        jouer au ping pong</p>
+      <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe  height="100%" src="https://giphy.com/embed/MHKl0gVYw0RTq" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-md-6 texte2">
+      <p>Quand tu es trop claqué pour finir tes quêtes et que tu envois ton
+        chien les faire à ta place</p>
+        <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe width="100%" height="100%" src="https://giphy.com/embed/SwImQhtiNA7io" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  <!-- 2nd ligne -->
+  <div class="row">
+    <div class="col-xs-12 col-md-6 texte3">
+      <p>Quand tu as enfin fini une quête avec cinq jours de retard</p>
+      <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe width="100%" height="100%" src="https://giphy.com/embed/rdma0nDFZMR32" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+    </div>
+
+
+    <div class="col-xs-12 col-md-6 texte4">
+      <p>Quand ton formateur te dit que tu as fait n'importe quoi</p>
+      <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe width="100%" height="100%" id="tuche" src="https://giphy.com/embed/kEE0sEMuovToXf1Ka5" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+    </div>
+  </div>
+
+  <!-- 3ieme ligne-->
+  <div class="row">
+    <div class="col-xs-12 col-md-6 texte5">
+      <p>Quand on t'annonce que ton 1er projet est à rendre dans 12 jours</p>
+      <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe width="100%" height="100%" src="https://giphy.com/embed/N8irTkug9DHDa" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-md-6 texte6">
+      <p>Quand c'est ton tour de présenter ton projet</p>
+      <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe width="100%" height="100%" src="https://giphy.com/embed/jUwpNzg9IcyrK" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+    </div>
+  </div>
+
+  <!-- 4ieme ligne-->
+  <div class="row">
+    <div class="col-xs-12 col-md-6">
+      <p>Quand ton formateur t'explique le fonctionnement de Git</p>
+      <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe width="100%" height="100%" src="https://giphy.com/embed/EE8a3k4aTmflK" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-md-6">
+      <p>Quand tu laisses la poubelle déborder avec des cannettes de coca pas totalement vide</p>
+      <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe width="100%" height="100%" src="https://giphy.com/embed/NFvIIedQhGTrq" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+    </div>
+  </div>
+    <!-- 5ieme ligne -->
+    <div class="row">
+      <div class="col-xs-12 col-md-6 ">
+        <div><p>Qui a jeté ses épluchures d'orange dans la mauvaise poubelle ?</p>
+        </div>
+        <div class="wrapper">
+          <img class="overlay" src="https://48f407d75247154c9493-acc40d89e93be9a91ac4d92a769c87f7.ssl.cf2.rackcdn.com/tv/tv-wrapper.png">
+          <div class="video">
+            <iframe width="100%" height="100%"  src="https://giphy.com/embed/3o7TKPdUkkbCAVqWk0" allowfullscreen frameborder="0"></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-md-6">
+
+      </div>
+    </div>
+  <footer>
+
+  </footer>
+
+
+
+</div>
+
+
+
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+</script>
+    <!-- Include all compiled plugins (below), or include individual files as
+   needed -->
+<script src="dist/js/bootstrap.min.js"></script>
+</body>
+</html>
